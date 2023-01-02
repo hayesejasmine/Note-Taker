59733269e5b258891d0fa7288313b5774b57d04b
hayesejasmine <hayesejasmine@gmail.com>
2023-01-02 15:17:31 -0500

    server.js updated

--- server.js
+++ server.js
@@ -15,10 +15,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./develop.public'));
 
+//GET request
+app.get('/api/notes', function(req, res) {
+    readFromFile("./develop/db.db.json", "utf8").then(function(data){
+notes = [].concat(JSON.parse(data))
+res.json(notes);
+    })
+});
+
+//POST request
 
 
+//DELETE request
 
 
+app.get('/notes', (req, res) => 
+res.sendFile(path.join(__dirname, './develop/public/notes.html'))
+);
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './develop/public/index.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, './develop/public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
